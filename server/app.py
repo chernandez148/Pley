8f0718f1e3d58c12fc4b68d456ba6d1a98a1f768
Christian <hernandezchristian94@gmail.com>
2023-04-17 16:23:08 -0700

    Initial commit

--- server/app.py
+++ server/app.py
@@ -1,16 +1,36 @@
 #!/usr/bin/env python3
 
 # Standard library imports
+import os
 
+from flask import Flask, jsonify, make_response
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
 # Remote library imports
 from flask import request
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
-from models import User, Recipe
+from models import db, User
 
-# Views go here!
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
+
+# Users go here!
+class Users(Resource):
+    
+    def get(self):
+        users = [user.to_dict() for user in User.query.all()]
+        return make_response(jsonify(users), 200)
+api.add_resource(Users, '/users')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/seed.py
+++ server/seed.py
@@ -1,17 +1,21 @@
-#!/usr/bin/env python3
-
-# Standard library imports
-from random import randint, choice as rc
-
-# Remote library imports
-from faker import Faker
-
-# Local imports
 from app import app
-from models import db
+from models import db, User
 
-if __name__ == '__main__':
-    fake = Faker()
-    with app.app_context():
-        print("Starting seed...")
-        # Seed code goes here!
+with app.app_context():
+
+    print('Deleting existing birds...')
+    User.query.delete()
+
+    print('Creating bird objects...')
+    user1 = User(fname='Chickadee', lname='Poecile', email='email@email.com')
+    user2 = User(fname='Grackle', lname='Quiscula', email="email1@email.com")
+    user3 = User(fname='Common', lname='Vulgaris', email="email2@email.com")
+    user4 = User(fname='Dove', lname='Zenaida', email="email3@email.com")
+
+    print('Adding bird objects to transaction...')
+    db.session.add_all([user1, user2, user3, user4])
+
+    print('Committing transaction...')
+    db.session.commit()
+
+    print('Complete.')
