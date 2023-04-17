c38c2b6e1043d8fe8a3a4979bd747444649a0eea
Katelyn Luther <katelynluther87@aim.com>
2023-04-17 17:37:33 -0600

    initial commit, user table

--- server/app.py
+++ server/app.py
@@ -3,12 +3,36 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
-from flask_restful import Resource
+import os
+
+from flask import Flask, jsonify, make_response, request
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
 
 # Local imports
 from config import app, db, api
-from models import User, Recipe
+from models import User
+
+from models import db, User
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
+
+class Users(Resource):
+    
+    def get(self):
+        users = [user.to_dict() for user in User.query.all()]
+        return make_response(jsonify(users), 200)
+    
+api.add_resource(Users, '/users')
+
 
 # Views go here!
 
