5173e49bf8c78e20a11110b0860426bf952d0d9e
Christian <hernandezchristian94@gmail.com>
2023-04-18 13:52:53 -0700

    update

--- server/models.py
+++ server/models.py
@@ -1,5 +1,7 @@
 from sqlalchemy_serializer import SerializerMixin
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
 
 db = SQLAlchemy()
 
@@ -8,12 +10,76 @@ from config import db
 # Models go here!
 
 class User(db.Model, SerializerMixin):
-    __tablename__ = 'users'
+    tablename = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String)
+    lname = db.Column(db.String)
+    email = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    reviews = db.relationship('Review', backref='user')
+    restaurants = association_proxy('reviews', 'restaurant')
+
+    def repr(self):
+        return f'Name: {self.fname} {self.lname} | Email: {self.email}'
+
+
+class Review(db.Model, SerializerMixin):
+    tablename = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    review = db.Column(db.String)
+    rating = db.Column(db.Integer)
+
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    def __repr__(self):
+        return f'Review: {self.review} | Rating: {self.rating} | restaurant_id: {self.restaurant_id} | user_id: {self.user_id}'
+
+    @validates('rating')
+    def validate_rating(self, key, rating):
+        if rating < 1 or rating > 5:
+            raise ValueError({'error': 'Rating must be between 1 and 5'})
+        return rating
+
+
+class Restaurant(db.Model, SerializerMixin):
+    tablename = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+
+    owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'))
+
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    reviews = db.relationship('Review', backref='restaurant')
+    users = association_proxy('reviews', 'user')
+
+    def __repr__(self):
+        return f'Name: {self.name} | owner_id: {self.owner_id}'
+
+
+class Owner(db.Model, SerializerMixin):
+    tablename = 'owners'
 
     id = db.Column(db.Integer, primary_key=True)
     fname = db.Column(db.String)
     lname = db.Column(db.String)
     email = db.Column(db.String)
 
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    restaurants = db.relationship('Restaurant', backref='owner')
+
     def __repr__(self):
         return f'Name: {self.fname} {self.lname} | Email: {self.email}'
+
--- server/seed.py
+++ server/seed.py
@@ -1,20 +1,50 @@
 #!/usr/bin/env python3
 from app import app
-from models import db, User
+from models import db, User, Review, Restaurant, Owner
 
 with app.app_context():
 
-    print('Deleting existing birds...')
+    print('Deleting existing data...')
     User.query.delete()
+    Review.query.delete()
+    Restaurant.query.delete()
+    Owner.query.delete()
 
-    print('Creating bird objects...')
+    print('Creating user objects...')
     user1 = User(fname='Black-Capped Chickadee', lname='Poecile Atricapillus', email='email@email.com')
     user2 = User(fname='Grackle', lname='Quiscalus Quiscula', email='email@email.com')
     user3 = User(fname='Common Starling', lname='Sturnus Vulgaris', email='email@email.com')
     user4 = User(fname='Mourning Dove', lname='Zenaida Macroura', email='email@email.com')
 
-    print('Adding bird objects to transaction...')
+    print('Creating review objects...')
+    review1 = Review(rating=4, review='Great food and service!', user_id=3, restaurant_id=10)
+    review2 = Review(rating=3, review='Decent food, but service was lacking.', user_id=4, restaurant_id=5)
+    review3 = Review(rating=2, review='Disappointing experience.', user_id=1, restaurant_id=6)
+    review4 = Review(id=4, rating=2, review='Disappointing experience.', user_id=1, restaurant_id=6)
+    review5 = Review(rating=4, review='Great ambiance and menu, but service was a bit slow.', user_id=8, restaurant_id=9)
+
+    print('Creating restaurant objects...')
+    restaurant1 = Restaurant(name='Pizza Palace', address='123 Main St', owner_id=1)
+    restaurant2 = Restaurant(name='Taco Town', address='456 Oak Ave', owner_id=2)
+    restaurant3 = Restaurant(name='Burger Barn', address='789 Elm St', owner_id=3)
+    restaurant4 = Restaurant(name='Sushi Spot', address='456 Pine St', owner_id=2)
+    restaurant5 = Restaurant(name='Pasta Place', address='234 Maple Ave', owner_id=4)
+
+    print('Creating ownser objects...')
+    owner1 = Owner(fname='John', lname='Doe', email='john.doe@example.com')
+    owner2 = Owner(fname='Jane', lname='Doe', email='jane.doe@example.com')
+    owner3 = Owner(fname='Bob', lname='Smith', email='bob.smith@example.com')
+    owner4 = Owner(fname='Alice', lname='Johnson', email='alice.johnson@example.com')
+    owner5 = Owner(fname='Michael', lname='Lee', email='michael.lee@example.com')
+
+
+
+    print('Adding data objects to transaction...')
+    db.session.add_all([owner1, owner2, owner3, owner4, owner5])
+    db.session.add_all([restaurant1, restaurant2, restaurant3, restaurant4, restaurant5])
+    db.session.add_all([review1, review2, review3, review4, review5])
     db.session.add_all([user1, user2, user3, user4])
+
 
     print('Committing transaction...')
     db.session.commit()
